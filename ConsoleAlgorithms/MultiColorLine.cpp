021830c9994b6b5c3c6925092d43ef95b44845f3
albel260819 <albel260819@gmail.com>
2025-03-05 18:41:01 +0300

    include colors for command line

---
+++ ConsoleAlgorithms/MultiColorLine.cpp
@@ -0,0 +1,120 @@
+
+#include "MultiColorLine.h"
+
+void mcl::setConsoleColor(HANDLE hConsole, int textColor, int backgroundColor)
+{
+    SetConsoleTextAttribute(hConsole, (backgroundColor << 4) | textColor);
+}
+
+mcl::cmd_color::cmd_color() : textColor(0), backgroundColor(0)
+{   }
+mcl::cmd_color::cmd_color(int textColor, int backgroundColor) : textColor(textColor), backgroundColor(backgroundColor)
+{   }
+
+
+mcl::letter::letter(double x, cmd_color color) : string(std::to_string(x)), color(color)
+{   }
+mcl::letter::letter(int x, cmd_color color) : string(std::to_string(x)), color(color)
+{   }
+mcl::letter::letter(std::string s, cmd_color color) : string(s), color(color)
+{   }
+
+mcl::letter mcl::operator * (std::string string, cmd_color color) {
+    return mcl::letter(string, color);
+}
+mcl::letter mcl::operator * (double number, cmd_color color) {
+    return letter(number, color);
+}
+mcl::letter mcl::operator * (int number, cmd_color color) {
+    return letter(number, color);
+}
+mcl::letter mcl::operator * (const char* string, cmd_color color) {
+    return mcl::letter(string, color);
+}
+enum tag
+{
+    tab,
+    endl,
+    nsep
+};
+
+mcl::print::print() : last_color(7, 0)
+{
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+}
+void mcl::print::add(letter str)
+{
+    string.push_back(str);
+}
+void mcl::print::out(tag tag_)
+{
+    for (auto it : string)
+    {
+#ifdef text_color
+        setConsoleColor(hConsole, it.color.textColor, it.color.backgroundColor);
+#endif // text_color
+        std::cout << it.string;
+    }
+#ifdef text_color
+    setConsoleColor(hConsole, 7, 0);
+#endif // text_color
+
+    switch (tag_)
+    {
+    case tab:
+        std::cout << "\t";
+        break;
+    case endl:
+        std::cout << "\n";
+        break;
+    case nsep:
+        break;
+    default:
+        break;
+    }
+    string.clear();
+}
+void mcl::print::SetPos(short x, short y)
+{
+    SetConsoleCursorPosition(hConsole, COORD{ x, y });
+}
+
+mcl::print& mcl::print::operator()(short x, short y)
+{
+    SetPos(x, y);
+    return *this;
+}
+
+
+mcl::print& mcl::operator << (print& out, tag tag_) {
+    out.out(tag_);
+    return out;
+}
+
+mcl::print& mcl::operator << (print& out, letter str) {
+    out.add(str);
+    out.last_color = str.color;
+    return out;
+}
+
+mcl::print& mcl::operator << (print& out, std::string str) {
+    out.add(letter(str, out.last_color));
+    return out;
+}
+
+mcl::print& mcl::operator << (print& out, const char* str) {
+    out.add(letter(str, out.last_color));
+    return out;
+}
+
+mcl::print& mcl::operator << (print& out, double num) {
+    out.add(letter(std::to_string(num), out.last_color));
+    return out;
+}
+
+mcl::print& mcl::operator << (print& out, int num) {
+    out.add(letter(std::to_string(num), out.last_color));
+    return out;
+}
+
+
---
+++ ConsoleAlgorithms/MultiColorLine.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include <windows.h>
+#include <cstdlib>
+
+
+#define text_color
+
+
+namespace mcl {
+    void setConsoleColor(HANDLE hConsole, int textColor, int backgroundColor);
+
+    struct cmd_color
+    {
+        int textColor;
+        int backgroundColor;
+        cmd_color();
+        cmd_color(int textColor, int backgroundColor);
+    };
+
+    struct letter {
+        std::string string;
+        cmd_color color;
+        letter(double x, cmd_color color);
+        letter(int x, cmd_color color);
+        letter(std::string s, cmd_color color);
+    };
+
+    letter operator * (std::string string, cmd_color color);
+    letter operator * (double number, cmd_color color);
+    letter operator * (int number, cmd_color color);
+    letter operator * (const char* string, cmd_color color);
+    enum tag
+    {
+        tab,
+        endl,
+        nsep
+    };
+
+    class print
+    {
+        HANDLE hConsole;
+        std::vector<letter> string;
+    public:
+        cmd_color last_color;
+
+        print();
+        void add(letter str);
+        void out(tag tag_);
+        void SetPos(short x, short y);
+
+        print& operator()(short x, short y);
+    };
+
+    print& operator << (print& out, tag tag_);
+    print& operator << (print& out, letter str);
+    print& operator << (print& out, std::string str);
+    print& operator << (print& out, const char* str);
+    print& operator << (print& out, double num);
+    print& operator << (print& out, int num);
+};
+
+
+/*
+0 = Черный
+1 = Синий
+2 = Зеленый
+3 = Голубой
+4 = Красный
+5 = Лиловый
+6 = Желтый
+7 = Белый
+8 = Серый
+9 = Светло-синий
+A/10 = Светло-зеленый
+B/11 = Светло-голубой
+C/12 = Светло-красный
+D/13 = Светло-лиловый
+E/14 = Светло-желтый
+F/15 = Ярко-белый
+*/
