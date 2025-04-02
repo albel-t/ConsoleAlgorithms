d50aa9b8efabcfac9b77406317f3906699e5320f
albel260819 <albel260819@gmail.com>
2025-04-02 11:54:01 +0300

    добавил вставку и удаление

--- ConsoleAlgorithms/laba_4.cpp
+++ ConsoleAlgorithms/laba_4.cpp
@@ -16,6 +16,9 @@ void l4task1()
 	str.start = new leaf<std::string>("str1");
 
 	str.pushBack("str2");
+	str.pushPtr(str.start, "str2.1");
+	str.print();
+	delete str.emplase(str.start);
 	str.print();
 
 
--- ConsoleAlgorithms/laba_4.h
+++ ConsoleAlgorithms/laba_4.h
@@ -49,31 +49,58 @@ public:
 	leaf<data_list_type>* start;
 	void pushBack(data_list_type data_leaf)
 	{
-		leaf<data_list_type>* new_leaf = new leaf<data_list_type>(data_leaf);
+		//leaf<data_list_type>* new_leaf = new leaf<data_list_type>(data_leaf);
 		leaf<data_list_type>* _leaf = start;
 		while (_leaf->next != nullptr)
 			_leaf = _leaf->next;
-		
-		new_leaf->before(_leaf->before(new_leaf));
+		pushPtr(_leaf, data_leaf);
+	}
+	void pushPtr(leaf<data_list_type>* push_after, data_list_type data_leaf)
+	{
+		leaf<data_list_type>* new_leaf = new leaf<data_list_type>(data_leaf);
+		leaf<data_list_type>* next_leaf = push_after->next;
 
-		if (_leaf->next != nullptr)
-		{
-			_leaf = _leaf->next;
-			new_leaf->after(_leaf->after(new_leaf));
-		}
-		else {
-			new_leaf->after(_leaf);
-		}
+
+		new_leaf->before(push_after->before(new_leaf));
+		if (next_leaf)
+			new_leaf->after(next_leaf->after(new_leaf));
+		else
+			new_leaf->after(push_after);
+
 	}
 	void print()
 	{
 		leaf<data_list_type>* _leaf = start;
 
-		while (_leaf != nullptr)
+		std::cout << _leaf->print() << std::endl;
+
+		while (_leaf->next != nullptr) 
 		{
-			std::cout << _leaf->print() << std::endl;
 			_leaf = _leaf->next;
+			std::cout << _leaf->print() << std::endl;
 		}
+		print_usd(_leaf);
+	}
+	void print_usd(leaf<data_list_type>* _leaf)
+	{
+		//leaf<data_list_type>* _leaf = start;
+		std::cout << _leaf->print() << std::endl;
+
+		while (_leaf->prev != nullptr)
+		{
+			_leaf = _leaf->prev;
+			std::cout << _leaf->print() << std::endl;
+		}
+	}
+	leaf<data_list_type>* emplase(leaf<data_list_type>* pop_after)
+	{
+		leaf<data_list_type>* next_pop_leaf = pop_after->next;
+		leaf<data_list_type>* next_leaf = next_pop_leaf->next;
+
+		pop_after->before(next_leaf);
+		next_leaf->after(pop_after);
+
+		return next_pop_leaf;
 	}
 };
 
